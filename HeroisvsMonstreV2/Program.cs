ac7f268f183bcb6e0e79fe3ab4e90acb74c74b4d
jordisanchoitb <jordi.sancho.7e7@itb.cat>
2024-01-15 18:44:39 +0100

    Selector dificultat fet, canviades variables closes #2

--- HeroisvsMonstreV2/Program.cs
+++ HeroisvsMonstreV2/Program.cs
@@ -15,32 +15,101 @@ namespace GameProject
     {
         public static void Main()
         {
-            // Mensajes para el Usuario
+            // Msg usuaris
             const string MsgWelcome = "Benginguts al joc Herois vs Monstre V2";
             const string MsgMenu = "1. Iniciar una nova batalla\n0. Sotir";
             const string MsgError = "Error, opcio incorrecta.\nQue vols fer?";
+            const string MsgExitGame = "Sortint del joc...";
+            const string MsgErrorMenu = $"Error, has superat el numero de intents permesos.\n{MsgExitGame}";
+            const string MsgLevelDificult = "Selecciona el nivell de dificultat:\n1. Facil\n2. Dificil\n3. Personalitzat\n4. Random";
+            const string MsgLevelChoiseEasy = "Has escollit el nivell facil";
+            const string MsgLevelChoiseHard = "Has escollit el nivell dificil";
+            const string MsgLevelChoiseCustomized = "Has escollit el nivell personalitzat";
+            const string MsgLevelChoiseRandom = "Has escollit el nivell random";
+
+            // Constants
+            const int MaxTries = 3;
+            const int OptionNewGame = 1;
+            const int OptionExitGame = 0;
+            const int LevelEasy = 1;
+            const int LevelHard = 2;
+            const int LevelCustomized = 3;
+            const int LevelRandom = 4;
+
 
             // Variables
             int userOption, trys = 0;
-            bool errormenu = false;
+            bool errorMenu = false;
 
-            // Inicio del programa
+            // Inici del programa
             Console.WriteLine(MsgWelcome);
             do
             {
-                if (errormenu)
+                if (errorMenu)
                 {
+                    Console.WriteLine();
                     Console.WriteLine(MsgError);
                 }
                 Console.WriteLine(MsgMenu);
                 userOption = Convert.ToInt32(Console.ReadLine());
                 trys++;
-                errormenu = true;
-            } while (!(userOption >= 0 && userOption <= 1) && trys != 3);
+                errorMenu = true;
+            } while (!(userOption >= OptionExitGame && userOption <= OptionNewGame) && trys != MaxTries);
             
+            if (Methods.ComprovUserOptionsAndTrys(userOption,OptionNewGame,trys))
+            {
+                errorMenu = false;
+                trys = 0;
+                do
+                {
+                    Console.WriteLine();
+                    if (errorMenu)
+                    {
+                        Console.WriteLine(MsgError);
+                    }
+                    Console.WriteLine(MsgLevelDificult);
+                    userOption = Convert.ToInt32(Console.ReadLine());
+                    trys++;
+                    errorMenu = true;
+                } while (!(userOption >= LevelEasy && userOption <= LevelRandom) && trys != MaxTries);
 
+                if (Methods.ComprovUserOptionsAndTrys(userOption, LevelEasy, trys))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(MsgLevelChoiseEasy);
+                    
+                }
+                else if (Methods.ComprovUserOptionsAndTrys(userOption, LevelHard, trys))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(MsgLevelChoiseHard);
+                    
+                }
+                else if (Methods.ComprovUserOptionsAndTrys(userOption, LevelCustomized, trys))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(MsgLevelChoiseCustomized);
+                    
+                }
+                else if (Methods.ComprovUserOptionsAndTrys(userOption, LevelRandom, trys))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(MsgLevelChoiseRandom);
 
-
+                }
+                else
+                {
+                    Console.WriteLine(MsgErrorMenu);
+                }
+            }
+            else if (userOption == OptionExitGame)
+            {
+                Console.WriteLine(MsgExitGame);
+            }
+            else
+            {
+                Console.WriteLine(MsgErrorMenu);
+            }
         }
     }
 }
--- MethodsHeroisvsMonstreV2/Class1.cs
+++ MethodsHeroisvsMonstreV2/Class1.cs
@@ -2,6 +2,10 @@
 {
     public static class Methods
     {
-
+        const int MaxTries = 3;
+        public static bool ComprovUserOptionsAndTrys(int userOption,int numbercomprovation ,int trys)
+        {
+            return userOption == numbercomprovation && trys != MaxTries || userOption == numbercomprovation && trys == MaxTries;
+        }
     }
 }
